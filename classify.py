d20b08f2c7da97f7d9538423c97f2fc50c16d2dc
wyx <l631068264@gmail.com>
2017-04-29 11:16:17 +0800

    classify.py

--- classify.py
+++ classify.py
@@ -85,5 +85,24 @@ def better_classify():
         print('correct={:<8} guess={:<8s} name={:<30}'.format(tag, guess, name))
 
 
-# classify()
-better_classify()
+def doc_classify():
+    from nltk.corpus import movie_reviews
+    documents = [(list(movie_reviews.words(fileid)), category)
+                 for category in movie_reviews.categories()
+                 for fileid in movie_reviews.fileids(category)]
+    random.shuffle(documents)
+    # 前 2000 个最频繁词的链表
+    all_words = nltk.FreqDist(w.lower() for w in movie_reviews.words())
+    word_features = list(all_words)[:2000]
+
+    def document_features(document):
+        document_words = set(document)
+        features = {}
+        for word in word_features:
+            features['contains({})'.format(word)] = (word in document_words)
+        return features
+
+    print(document_features(movie_reviews.words('pos/cv957_8737.txt')))
+
+
+doc_classify()
